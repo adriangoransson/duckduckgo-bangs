50f330ff25ff268e0e9ff20e6cd6b46b3fe477a7
Adrian Göransson <adriangoransson@gmail.com>
2020-03-22 21:13:37 +0100

    Ditch domvm for lighterhtml and support reordering of bangs
    * support for dark color scheme

--- extension/options.js
+++ extension/options.js
@@ -1,5 +1,5 @@
-const { domvm } = window;
-const el = domvm.defineElement;
+/* eslint-disable no-use-before-define, no-param-reassign */
+const { lighterhtml: { render, html } } = window;
 
 function save(settings) {
   browser.storage.sync.set({ settings });
@@ -9,110 +9,108 @@ function restore() {
   return browser.storage.sync.get('settings');
 }
 
-function ListView(vm, data) {
-  const settings = data;
+restore().then(({ settings }) => {
+  options(document.body, settings);
+});
+
+function options(node, settings) {
+  const { length } = settings.bangs;
+  const rerender = () => options(node, settings);
+
+  render(node, html`
+    <h2>Behaviour</h2>
+
+    <table>
+      <tr>
+        <td>
+          <label for="check-new-tab">
+            Open search in new tab
+          </label>
+          </td>
+          <td>
+            <input checked=${settings.newTab} onchange=${toggleNewTab} id="check-new-tab" type="checkbox">
+          </td>
+      </tr>
+
+      <tr>
+        <td>
+          <label for="check-tab-background">
+            ↪ Open new tab in background
+          </label>
+          </td>
+          <td>
+            <input checked=${settings.backgroundTab} onchange=${toggleBackgroundTab} id="check-tab-background" type="checkbox">
+          </td>
+      </tr>
+    </table>
+
+    <h2>Context menu bangs</h2>
+
+    <table>
+      <tr>
+        <th>Display name</th>
+        <th>DDG bang</th>
+      </tr>
+
+      ${settings.bangs.map(({ display, bang }, i) => html`
+        <tr>
+          <td><input oninput=${(e) => update(i, 'display', e.target.value)} value=${display}></td>
+          <td><input oninput=${(e) => update(i, 'bang', e.target.value)} value=${bang}></td>
+          <td><button onclick=${() => moveUp(i)} disabled=${i === 0}>⬆</button></td>
+          <td><button onclick=${() => moveDown(i)} disabled=${i === length - 1}>⬇</button></td>
+          <td><button disabled=${length === 1} onclick=${() => remove(i)}>Remove</button></td>
+        </tr>
+      `)}
+
+    </table>
+
+    <p>
+      <button onclick=${add}>Add</button>
+    </p>
+  `);
 
   function toggleNewTab() {
     settings.newTab = !settings.newTab;
-    save(settings);
-    vm.redraw();
+    rerender();
   }
 
   function toggleBackgroundTab() {
     settings.backgroundTab = !settings.backgroundTab;
-    save(settings);
+    rerender();
   }
 
-  function delItem(i) {
+  function update(i, key, value) {
+    settings.bangs[i][key] = value;
+    rerender();
+  }
+
+  function move(oldIndex, newIndex) {
+    const temp = settings.bangs[oldIndex];
+    settings.bangs[oldIndex] = settings.bangs[newIndex];
+    settings.bangs[newIndex] = temp;
+    rerender();
+  }
+
+  function moveUp(i) {
+    move(i, i - 1);
+  }
+
+  function moveDown(i) {
+    move(i, i + 1);
+  }
+
+  function remove(i) {
     settings.bangs.splice(i, 1);
-    save(settings);
-    vm.redraw();
+    rerender();
   }
 
-  function addItem() {
-    const display = vm.refs.newDisplay.el;
-    const bang = vm.refs.newBang.el;
+  function add() {
+    const bangs = settings.bangs.filter(({ display, bang }) => display || bang);
+    bangs.push({ display: '', bang: '' });
 
-    if (display.value !== '') {
-      settings.bangs.push({ display: display.value, bang: bang.value });
-      display.value = '';
-      bang.value = '';
-      save(settings);
-      vm.redraw();
-    }
+    settings.bangs = bangs;
+    rerender();
   }
 
-  function updateItem(i, bang, display) {
-    settings.bangs[i] = { bang, display };
-    save(settings);
-    vm.redraw();
-  }
-
-  function updateDisplay(i, bang, e, node) {
-    updateItem(i, bang, node.el.value);
-  }
-
-  function updateBang(i, display, e, node) {
-    updateItem(i, node.el.value, display);
-  }
-
-  return () =>
-    el('div', [
-      el('h2', 'Preferences'),
-
-      el('p', [
-        el('input#new-tab', { type: 'checkbox', checked: data.newTab, onchange: [toggleNewTab] }),
-        el('label', { for: 'new-tab' }, 'Open search in new tab'),
-      ]),
-
-      el('p', [
-        el('input#background-tab', {
-          type: 'checkbox',
-          disabled: !data.newTab,
-          checked: data.backgroundTab,
-          onchange: [toggleBackgroundTab],
-        }),
-        el('label', { for: 'background-tab' }, 'Open new tab in background'),
-      ]),
-
-      el('h2', 'DuckDuckGo bangs'),
-
-      el('table', [
-        el('tr', [
-          el('th', 'Display Name'),
-          el('th', 'DDG Bang'),
-          el('th', 'Action'),
-        ]),
-
-        settings.bangs.map((item, i) => el('tr', [
-          el('td.browser-style', [
-            el('input', { value: item.display, onkeyup: [updateDisplay, i, item.bang] }),
-          ]),
-          el('td.browser-style', [
-            el('input', { value: item.bang, onkeyup: [updateBang, i, item.display] }),
-          ]),
-          el('td', [
-            el('button.browser-style', { onclick: [delItem, i] }, 'Remove'),
-          ]),
-        ])),
-
-        el('tr', [
-          el('td.browser-style', [
-            el('input', { _ref: 'newDisplay' }),
-          ]),
-          el('td.browser-style', [
-            el('input', { _ref: 'newBang' }),
-          ]),
-          el('td', [
-            el('button.browser-style', { onclick: addItem }, 'Add'),
-          ]),
-        ]),
-      ]),
-    ]);
+  save(settings);
 }
-
-restore().then((data) => {
-  const { settings } = data;
-
-  domvm.createView(ListView, settings).mount(document.body);
-});
